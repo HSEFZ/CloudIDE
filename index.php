9a11081b23fa15f42362a621a2fbe6ae126c2b92
jcpwfloi <jcpwfloi@gmail.com>
2014-01-29 02:06:18 +0000

    basic css and index added

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Welcome to CloudIDE!</title>
+	<link rel="stylesheet" href="css/index.css" type="text/css" />
+</head>
+<body>
+	<div class="container">
+		<h1>
+			Welcome to Cloud IDE!
+		</h1>
+		<center><div class="button">Getting Started!</center>
+	</div>
+</body>
+</html>
