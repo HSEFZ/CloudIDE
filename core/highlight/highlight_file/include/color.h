a3666b703ccca5c41d3ed75a2e40b733b9821451
Jeffery Liu <dilutedream@gmail.com>
2014-01-29 23:12:18 +0800

    some modification in color.h

--- core/highlight/highlight_file/include/color.h
+++ core/highlight/highlight_file/include/color.h
@@ -3,6 +3,24 @@ inline int read_color (std::string const& location) {
 	
 	FIN* color = fopen (real_location.c_str (), "r");
 	
+	char div_name [LEN_MAX];
+	
+	while (fscanf (color, "%s\n", div_name) != EOF) {
+		div_number [div_name] = div_count;
+		
+		while (true) {
+			std::string line = fgetline (color);
+			
+			if (line != "") {
+				/**	UNFINISHED	--Read the details of the line.	*/
+			} else {
+				break;
+			}
+		}
+		
+		div_count ++;
+	}
+	
 	fclose (color);
 }
 
--- core/highlight/highlight_file/include/variable.h
+++ core/highlight/highlight_file/include/variable.h
@@ -1,1 +1,9 @@
+struct word {
+	std::string data;
+	int type, div;
+};
 
+int div_count;
+
+std::map < std::string, int > div_number;
+std::vector < std::vector < word > > word_list;
