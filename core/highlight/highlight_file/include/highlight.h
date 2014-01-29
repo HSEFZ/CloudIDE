040faeb7b36c29beae7b916c0a8f4d35e74dd924
Jeffery Liu <dilutedream@gmail.com>
2014-01-30 00:41:54 +0800

    Some preparations for highlight.h

--- core/highlight/highlight_file/include/highlight.h
+++ core/highlight/highlight_file/include/highlight.h
@@ -2,7 +2,13 @@ inline void print_highlight (std::string const& input_filename, std::string cons
 	FILE* input = fopen (input_filename.c_str (), "r");
 	FILE* output = fopen (output_filename.c_str (), "w");
 	
-	
+	std::string line;
+	 
+	while (fgetline (input, line)) {
+		/**	UNFINISHED	--AC Automata	*/
+		
+		/**	UNFINISHED	--scan the string (line)	*/ 
+	}
 	
 	fclose (input);
 	fclose (output); 
