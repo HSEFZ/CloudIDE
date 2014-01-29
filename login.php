8abff098c1b431650913875b5624241b2c479197
jcpwfloi <jcpwfloi@gmail.com>
2014-01-29 07:12:11 +0000

    login module

--- login.php
+++ login.php
@@ -11,9 +11,23 @@
 
 <head>
 	<meta charset="utf-8" />
+	<link rel="stylesheet" href="css/index.css" type="text/css" />
+	<link rel="stylesheet" href="css/login.css" type="text/css" />
 </head>
 
 <body>
+	<div class="container">
+		<h1>
+			Welcome to Cloud IDE!	
+		</h1>
+		<div class="loginwindow">
+			<div class="loginwindowhead">
+			</div>
+			<div class="smalltitle">
+				Please Login or <a href="#">register a new account</a>
+			</div>
+		</div>
+	</div>
 </body>
 
 </html>
