cb7cd4e5e2dcbee98fe49aab50119fb53df27bac
jcpwfloi <jcpwfloi@gmail.com>
2014-01-30 14:21:10 +0000

    admin page modification

---
+++ admin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+
+<html>
+<head>
+	<link rel="stylesheet" href="css/index.css" type="text/css" />
+	<link rel="stylesheet" href="css/navigator.css" type="text/css" />
+</head>
+<body>
+<div class="container">
+	<div class="navigator">
+		<div class="navigator-button">
+			Home
+		</div>
+		<div class="navigator-button">
+			Admin_Home
+		</div>
+		<div class="navigator-button">
+			Environment
+		</div>
+		<div class="navigator-button">
+			Users&Groups
+		</div>
+	</div>
+</div>
+</body>
+</html>
