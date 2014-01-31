f0f4a1062688f44be4ffc147d1082a3f1b173738
jcpwfloi <jcpwfloi@gmail.com>
2014-01-31 20:35:44 +0800

    admin panel css configuration

--- admin.php
+++ admin.php
@@ -49,30 +49,32 @@
 				</li>
 			</ul>
 		</div>
-		<div class="content">
-			<div class="tag">
-				<div class="tagtitle">Server Status</div>
-				<div class="tagcontent">
+		<div class="admincontainer">
+			<div class="content">
+				<div class="tag">
+					<div class="tagtitle">Server Status</div>
+					<div class="tagcontent">
+					</div>
 				</div>
-			</div>
-			<div class="tag">
-				<div class="tagtitle">Environment Options</div>
-				<div class="tagcontent">
+				<div class="tag">
+					<div class="tagtitle">Environment Options</div>
+					<div class="tagcontent">
+					</div>
 				</div>
-			</div>
-			<div class="tag">
-				<div class="tagtitle">MySQL Configuration</div>
-				<div class="tagcontent">
+				<div class="tag">
+					<div class="tagtitle">MySQL Configuration</div>
+					<div class="tagcontent">
+					</div>
 				</div>
-			</div>
-			<div class="tag">
-				<div class="tagtitle">Stop/Start/Restart</div>
-				<div class="tagcontent">
+				<div class="tag">
+					<div class="tagtitle">Stop/Start/Restart</div>
+					<div class="tagcontent">
+					</div>
 				</div>
-			</div>
-			<div class="tag">
-				<div class="tagtitle">Plug-ins</div>
-				<div class="tagcontent">
+				<div class="tag">
+					<div class="tagtitle">Plug-ins</div>
+					<div class="tagcontent">
+					</div>
 				</div>
 			</div>
 		</div>
